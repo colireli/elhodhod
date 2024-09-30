8f4072958895189d58bda89e3f59be49d2b00c4c
opestro <mahdiharzallah21@gmail.com>
2024-09-30 22:38:13 +0100

    fix permission and folder cp v2

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,7 @@ while ! mysqladmin ping -h"$DB_HOST" --silent; do
     echo "Waiting for database connection..."
     sleep 2
 done
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
 
 # Run migrations
 php artisan migrate --force
